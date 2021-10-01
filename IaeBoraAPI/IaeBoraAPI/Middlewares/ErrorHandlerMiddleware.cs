6a96f118d3982cd4b1f46ccab628318992ea5d4d
Lucas Nakagawa <lucasnakagawanascimento@gmail.com>
2021-09-30 23:37:10 -0300

    feat: Add Middleware.

---
+++ IaeBoraAPI/IaeBoraAPI/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace IaeBoraAPI.Middlewares
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var resultjson = JsonConvert.SerializeObject(new { message = exception.Message });
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            
+            return context.Response.WriteAsync(resultjson);
+        }
+    }
+}
