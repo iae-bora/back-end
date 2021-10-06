e11e703fdd96e9a3a7a20cb2a4e48638f8a261a3
Lucas Nakagawa <lucasnakagawanascimento@gmail.com>
2021-10-05 23:48:51 -0300

    feat: Add MigrationV0

--- IaeBoraAPI/IaeBoraLibrary/Migrations/20211006024423_MigrationDB_V0.Designer.cs
+++ IaeBoraAPI/IaeBoraLibrary/Migrations/20211006024423_MigrationDB_V0.Designer.cs
@@ -4,14 +4,16 @@ using IaeBoraLibrary.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IaeBoraLibrary.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20211006024423_MigrationDB_V0")]
+    partial class MigrationDB_V0
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
